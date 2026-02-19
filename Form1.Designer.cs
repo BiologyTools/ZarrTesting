16f0e146d628df42e7585533b4c1d89ba14cbf6c
Erik Repo <84872500+BiologyTools@users.noreply.github.com>
2026-02-19 22:39:36 +0200

    Testing form.

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Zarr
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            openURLToolStripMenuItem = new ToolStripMenuItem();
+            folderBrowserDialog = new FolderBrowserDialog();
+            pictureBox = new PictureBox();
+            openFileDialog = new OpenFileDialog();
+            trackBar1 = new TrackBar();
+            trackBar2 = new TrackBar();
+            trackBar3 = new TrackBar();
+            panel1 = new Panel();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar3).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(521, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, openURLToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            fileToolStripMenuItem.Click += fileToolStripMenuItem_Click;
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(127, 22);
+            openToolStripMenuItem.Text = "Open File";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // openURLToolStripMenuItem
+            // 
+            openURLToolStripMenuItem.Name = "openURLToolStripMenuItem";
+            openURLToolStripMenuItem.Size = new Size(127, 22);
+            openURLToolStripMenuItem.Text = "Open URL";
+            openURLToolStripMenuItem.Click += openURLToolStripMenuItem_Click;
+            // 
+            // pictureBox
+            // 
+            pictureBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox.Location = new Point(0, 27);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(516, 249);
+            pictureBox.TabIndex = 1;
+            pictureBox.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            trackBar1.Location = new Point(4, 105);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(509, 45);
+            trackBar1.TabIndex = 2;
+            trackBar1.TickStyle = TickStyle.None;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
+            // trackBar2
+            // 
+            trackBar2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            trackBar2.Location = new Point(4, 3);
+            trackBar2.Name = "trackBar2";
+            trackBar2.Size = new Size(509, 45);
+            trackBar2.TabIndex = 3;
+            trackBar2.TickStyle = TickStyle.None;
+            // 
+            // trackBar3
+            // 
+            trackBar3.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            trackBar3.Location = new Point(4, 54);
+            trackBar3.Name = "trackBar3";
+            trackBar3.Size = new Size(509, 45);
+            trackBar3.TabIndex = 4;
+            trackBar3.TickStyle = TickStyle.None;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.Controls.Add(trackBar2);
+            panel1.Controls.Add(trackBar1);
+            panel1.Controls.Add(trackBar3);
+            panel1.Location = new Point(0, 282);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(516, 163);
+            panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(521, 447);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar3).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private FolderBrowserDialog folderBrowserDialog;
+        private OpenFileDialog openFileDialog;
+        private PictureBox pictureBox;
+        private TrackBar trackBar1;
+        private TrackBar trackBar2;
+        private TrackBar trackBar3;
+        private ToolStripMenuItem openURLToolStripMenuItem;
+        private Panel panel1;
+    }
+}
